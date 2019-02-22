da0f734d7b6bd318874da2564a2bb35229c122bc
Lorién <lorienportella@gmail.com>
2019-02-22 12:29:06 +0100

    class 1 2.0

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np # Imports numpy under alias np
+import cv2
 
 arr = np.zeros(10)
 
@@ -63,3 +64,21 @@ arr = np.ones((10,10))
 arr[1:-1,1:-1] = 0
 
 print(arr)
+
+#exercise 17
+arr = np.zeros((64,64))
+gradient = np.arange(0.0,64.0)
+gradient = gradient / 63.0
+gradient = gradient *255.0
+arr+=gradient
+arr = np.uint8(arr)
+cv2.imshow("Image",arr)
+
+
+#exercise 18
+gradient = np.reshape(gradient,(64,1))
+arr = np.zeros((64,64))
+arr+=gradient
+arr = np.uint8(arr)
+cv2.imshow("Image2",arr)
+cv2.waitKey(0)
